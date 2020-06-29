c64340d8e498390e6974d479d6f1a216d489aabd
thieryl <thiery.louison@gmail.com>
2020-06-29 11:33:43 +0400

    1. Why is this change neccesary? test

--- config.el
+++ config.el
@@ -30,7 +30,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-(setq org-agenda-files (quote ("~/play/org")
+(setq org-agenda-files (quote ("~/play/org")))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.

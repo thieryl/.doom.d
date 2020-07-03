33d0cbbfe9b2859fa33a0565c50029c9b71188b6
T10 <tlouison@dsogroup.com>
2020-07-03 14:18:01 +0400

    Add separate keyboard shortcuts

---
+++ keybr.custom.el
@@ -0,0 +1,9 @@
+;;; ~/.doom.d/keybr.custom.el -*- lexical-binding: t; -*-
+
+;;(global-set-key [f8] 'treemacs/toggle)
+(global-set-key (kbd "C-x k") 'my-kill-buffer)
+(global-set-key (kbd "C-x K") 'my-kill-buffer-and-jump)
+(global-set-key (kbd "C-x O") 'my-prev-window)
+(global-set-key (kbd "C-x c") 'magit-clone)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key [f12] 'fci-mode)
